f28358d8e532db63fb79a1e6d3eaa04d2eb0648d
Parthiban UthayaKumar <uparthiban@outlook.com>
2021-01-27 20:13:53 +0530

    added Startup WeatherData

--- WeatherData/Startup.cs
+++ WeatherData/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 
 namespace ShoeCompany
 {
@@ -26,12 +29,14 @@ namespace ShoeCompany
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddControllers();
 
             // Configure Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Weather Data",
                     Version = "v1",
@@ -39,26 +44,35 @@ namespace ShoeCompany
                 });
 
                 // Configure Swagger to use the xml documentation file
-                //var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
-                //c.IncludeXmlComments(xmlFile);
+                var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
+                c.IncludeXmlComments(xmlFile);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
            
-                app.UseDeveloperExceptionPage();
-            
-
-            app.UseMvc();
+            app.UseDeveloperExceptionPage();
+            //app.UseMvc();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
             app.UseSwagger(c =>
             {
+                /*
                 c.PreSerializeFilters.Add((swaggerDoc, httpReq) => {
                     swaggerDoc.Host = httpReq.Host.Value;
                     swaggerDoc.Schemes = new List<string>() { httpReq.Scheme };
                     swaggerDoc.BasePath = "/" + httpReq.PathBase;
+                });
+                */
+                c.PreSerializeFilters.Add((swagger, httpReq) =>
+                {
+                    swagger.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" } };
                 });
             });
             app.UseSwaggerUI(c =>
